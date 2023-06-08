f8cd39f21febcf215acb373e4a86cc005c94cc7e
Rafhael <rafhael.dev@gmail.com>
2023-06-08 18:19:40 -0300

    fix migrations

--- src/main/resources/db/tests/afterMigrate.sql
+++ src/main/resources/db/tests/afterMigrate.sql
@@ -51,9 +51,9 @@ values (3, 'tuk tuk indian food', 15, 2, current_timestamp, current_timestamp, t
 insert into tab_restaurants (id, name, shipping_costs, kitchen_id, created_at, updated_at, active)
 values (4, 'java steakhouse', 12, 3, current_timestamp, current_timestamp, true);
 insert into tab_restaurants (id, name, shipping_costs, kitchen_id, created_at, updated_at, active)
-values (5, 'Uncle Sam\'s Diner', 11, 4, current_timestamp, current_timestamp, true);
-insert into restaurante (id, name, shipping_costs, kitchen_id, created_at, updated_at, active)
-values (6, 'Mary\'s Bar', 6, 4, current_timestamp, current_timestamp, true);
+values (5, 'Uncle Sams Diner', 11, 4, current_timestamp, current_timestamp, true);
+insert into tab_restaurants (id, name, shipping_costs, kitchen_id, created_at, updated_at, active)
+values (6, 'Marys Bar', 6, 4, current_timestamp, current_timestamp, true);
 
 insert into tab_payment_ways (id, description) values (1, 'Credit Card');
 insert into tab_payment_ways (id, description) values (2, 'Debit Card');
@@ -64,12 +64,12 @@ insert into tab_permissions (id, name, description) values (2, 'UPDATE_KITCHEN',
 
 insert into restaurant_payment_way (restaurant_id, paymentway_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3), (4, 1), (4, 2), (5, 1), (5, 2), (6, 3);
 
-insert into tab_products (name, description, price, active, restaurant_id) values ('Pork with sweet and sour sauce', 'Delicious pork with special sauce', 78.90, 1, 1);
-insert into tab_products (name, description, price, active, restaurant_id) values ('Thai shrimp', '16 large prawns in spicy sauce', 110, 1, 1);
-insert into tab_products (name, description, price, active, restaurant_id) values ('Spicy salad with grilled meat', 'Leaf salad with thinly sliced grilled beef and our special red pepper sauce', 87.20, 1, 2);
-insert into tab_products (name, description, price, active, restaurant_id) values ('Garlic Naan', 'Indian traditional bread with garlic topping', 21, 1, 3);
-insert into tab_products (name, description, price, active, restaurant_id) values ('Murg Curry', 'Cubes of chicken prepared with curry sauce and spices', 43, 1, 3);
-insert into tab_products (name, description, price, active, restaurant_id) values ('Ancho steak', 'Tender and juicy cut, two fingers thick, taken from the front of the sirloin steak', 79, 1, 4);
-insert into tab_products (name, description, price, active, restaurant_id) values ('T-Bone', 'Very tasty cut, with a T-shaped bone, with the sirloin on one side and the filet mignon on the other', 89, 1, 4);
-insert into tab_products (name, description, price, active, restaurant_id) values ('X-All sandwich', 'Big sandwich with lots of cheese, beef burger, bacon, egg, salad and mayonnaise', 19, 1, 5);
-insert into tab_products (name, description, price, active, restaurant_id) values ('termite skewer', 'Comes with flour, cassava and vinaigrette', 8, 1, 6);
+insert into tab_products (name, description, price, active, restaurant_id, created_at, updated_at) values ('Pork with sweet and sour sauce', 'Delicious pork with special sauce', 78.90, true, 1, current_timestamp, current_timestamp);
+insert into tab_products (name, description, price, active, restaurant_id, created_at, updated_at) values ('Thai shrimp', '16 large prawns in spicy sauce', 110, true, 1, current_timestamp, current_timestamp);
+insert into tab_products (name, description, price, active, restaurant_id, created_at, updated_at) values ('Spicy salad with grilled meat', 'Leaf salad with thinly sliced grilled beef and our special red pepper sauce', 87.20, true, 2, current_timestamp, current_timestamp);
+insert into tab_products (name, description, price, active, restaurant_id, created_at, updated_at) values ('Garlic Naan', 'Indian traditional bread with garlic topping', 21, true, 3, current_timestamp, current_timestamp);
+insert into tab_products (name, description, price, active, restaurant_id, created_at, updated_at) values ('Murg Curry', 'Cubes of chicken prepared with curry sauce and spices', 43, true, 3, current_timestamp, current_timestamp);
+insert into tab_products (name, description, price, active, restaurant_id, created_at, updated_at) values ('Ancho steak', 'Tender and juicy cut, two fingers thick, taken from the front of the sirloin steak', 79, true, 4, current_timestamp, current_timestamp);
+insert into tab_products (name, description, price, active, restaurant_id, created_at, updated_at) values ('T-Bone', 'Very tasty cut, with a T-shaped bone, with the sirloin on one side and the filet mignon on the other', 89, true, 4, current_timestamp, current_timestamp);
+insert into tab_products (name, description, price, active, restaurant_id, created_at, updated_at) values ('X-All sandwich', 'Big sandwich with lots of cheese, beef burger, bacon, egg, salad and mayonnaise', 19, true, 5, current_timestamp, current_timestamp);
+insert into tab_products (name, description, price, active, restaurant_id, created_at, updated_at) values ('termite skewer', 'Comes with flour, cassava and vinaigrette', 8, true, 6, current_timestamp, current_timestamp);
