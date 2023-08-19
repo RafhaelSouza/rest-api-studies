6b8b8aa9b602bcfd529a6ebf2b044674c3390741
Rafhael <rafhael.dev@gmail.com>
2023-08-19 19:15:07 -0300

    feat: adding the filters in the daily sales query

--- src/main/java/com/studies/foodorders/infrastructure/repositories/sells/SellsRepositoryImpl.java
+++ src/main/java/com/studies/foodorders/infrastructure/repositories/sells/SellsRepositoryImpl.java
@@ -3,12 +3,14 @@ package com.studies.foodorders.infrastructure.repositories.sells;
 import com.studies.foodorders.domain.filter.DailySellsFilter;
 import com.studies.foodorders.domain.models.order.Order;
 import com.studies.foodorders.domain.models.order.dto.DailySells;
+import com.studies.foodorders.domain.models.order.enumerations.OrderStatus;
 import com.studies.foodorders.domain.repositories.sells.SellsRepositoryQueries;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.Date;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -22,6 +24,7 @@ public class SellsRepositoryImpl implements SellsRepositoryQueries {
 		var builder = manager.getCriteriaBuilder();
 		var query = builder.createQuery(DailySells.class);
 		var root = query.from(Order.class);
+		var predicates = new ArrayList<Predicate>();
 
 		//var functionDateCreationDate = builder.function("cast", Date.class, root.get("createdAt"), builder.literal("date"));
 
@@ -30,7 +33,25 @@ public class SellsRepositoryImpl implements SellsRepositoryQueries {
 				builder.count(root.get("id")),
 				builder.sum(root.get("totalPrice")));
 
+		if (filter.getRestaurantId() != null) {
+			predicates.add(builder.equal(root.get("restaurant"), filter.getRestaurantId()));
+		}
+
+		if (filter.getCreationDateStart() != null) {
+			predicates.add(builder.greaterThanOrEqualTo(root.get("createdAt"),
+					filter.getCreationDateStart()));
+		}
+
+		if (filter.getCreationDateEnd() != null) {
+			predicates.add(builder.lessThanOrEqualTo(root.get("createdAt"),
+					filter.getCreationDateEnd()));
+		}
+
+		predicates.add(root.get("status").in(
+				OrderStatus.CONFIRMED, OrderStatus.DELIVERED));
+
 		query.select(selection);
+		query.where(predicates.toArray(new Predicate[0]));
 		query.groupBy(root.get("createdAt"));
 
 		return manager.createQuery(query).getResultList();
