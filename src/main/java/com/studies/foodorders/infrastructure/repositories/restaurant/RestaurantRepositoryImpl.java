369e54a351cdad58ef4d3e5a96a347b7cbaabeb5
Rafhael <rafhael.dev@gmail.com>
2023-08-25 08:21:14 -0300

    refactor: shipping costs calculate

--- src/main/java/com/studies/foodorders/infrastructure/repositories/restaurant/RestaurantRepositoryImpl.java
+++ src/main/java/com/studies/foodorders/infrastructure/repositories/restaurant/RestaurantRepositoryImpl.java
@@ -10,14 +10,9 @@ import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import static com.studies.foodorders.infrastructure.repositories.restaurant.specifications.RestaurantSpecs.withFreeShipping;
@@ -36,56 +31,30 @@ public class RestaurantRepositoryImpl implements RestaurantRepositoryQueries {
     @Override
     public List<Restaurant> find(String name, BigDecimal initialCost, BigDecimal finalCost) {
 
-        var jpql = new StringBuilder();
-        jpql.append("from Restaurant where 1 = 1 ");
+        var builder = entityManager.getCriteriaBuilder();
 
-        var parameters = new HashMap<String, Object>();
+        var criteria = builder.createQuery(Restaurant.class);
+        var root = criteria.from(Restaurant.class);
 
-        if (StringUtils.hasLength(name)) {
-            jpql.append("and name like :name ");
-            parameters.put("name", "%" + name + "%");
+        var predicates = new ArrayList<Predicate>();
+
+        if (StringUtils.hasText(name)) {
+            predicates.add(builder.like(root.get("name"), "%" + name + "%"));
         }
 
         if (initialCost != null) {
-            jpql.append("shippingCosts >= :initialCost ");
-            parameters.put("initialCost", initialCost);
+            predicates.add(builder.greaterThanOrEqualTo(root.get("shippingCosts"), initialCost));
         }
 
         if (finalCost != null) {
-            jpql.append("shippingCosts <= :finalCost ");
-            parameters.put("finalCost", finalCost);
+            predicates.add(builder.lessThanOrEqualTo(root.get("shippingCosts"), finalCost));
         }
 
-        TypedQuery<Restaurant> query = entityManager.createQuery(jpql.toString(), Restaurant.class);
+        criteria.where(predicates.toArray(new Predicate[0]));
 
-        parameters.forEach((key, value) -> query.setParameter(key, value));
+        var query = entityManager.createQuery(criteria);
 
-        //return query.getResultList();
-
-        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
-
-        CriteriaQuery<Restaurant> criteria = builder.createQuery(Restaurant.class);
-        Root<Restaurant> root = criteria.from(Restaurant.class);
-
-        List<Predicate> conditionals = new ArrayList<>();
-
-        if (StringUtils.hasLength(name)) {
-            conditionals.add(builder.like(root.get("name"), "%" + name + "%"));
-        }
-        if (initialCost != null) {
-            conditionals.add(builder.greaterThanOrEqualTo(root.get("shippingCosts"), initialCost));
-        }
-        if (finalCost != null) {
-            conditionals.add(builder.lessThanOrEqualTo(root.get("shippingCosts"), finalCost));
-        }
-
-        criteria.where(conditionals.toArray(new Predicate[0]));
-
-        TypedQuery<Restaurant> queryCriteria = entityManager.createQuery(criteria);
-
-        //return queryCriteria.getResultList();
-
-        return null;
+        return query.getResultList();
 
     }
 
