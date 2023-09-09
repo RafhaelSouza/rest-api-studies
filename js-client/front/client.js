ebc0b5f5510409bf928d2875cefa31d00741b83f
Rafhael <rafhael.dev@gmail.com>
2023-09-09 15:14:00 -0300

    implementing a registration form and making post requests with javascript

--- js-client/front/client.js
+++ js-client/front/client.js
@@ -13,7 +13,7 @@ function tableFill(paymentWays) {
   $("#my-table tbody tr").remove();
 
   $.each(paymentWays, function(i, paymentWay) {
-    var line = $("<tr>");
+    let line = $("<tr>");
 
     line.append(
       $("<td>").text(paymentWay.id),
@@ -24,4 +24,34 @@ function tableFill(paymentWays) {
   });
 }
 
+function registerPaymentWay() {
+  let paymentWayJson = JSON.stringify({
+    "description": $("#description-field").val()
+  });
+
+  console.log(paymentWayJson);
+
+  $.ajax({
+    url: "http://localhost:8080/payment-ways",
+    type: "post",
+    data: paymentWayJson,
+    contentType: "application/json",
+
+    success: function(response) {
+      alert("Payment method added!");
+      findPaymentWay();
+    },
+
+    error: function(error) {
+      if (error.status == 400) {
+        let problem = JSON.parse(error.responseText);
+        alert(problem.user_message);
+      } else {
+        alert("Error registering payment method!");
+      }
+    }
+  });
+}
+
 $("#btn-find").click(findPaymentWay);
+$("#btn-send").click(registerPaymentWay);
