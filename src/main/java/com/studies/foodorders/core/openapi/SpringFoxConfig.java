7f43d4778944b44fdc1d19747275ca1bdce702cc
Rafhael <rafhael.dev@gmail.com>
2023-09-14 07:51:42 -0300

    feat: describing api information in documentation

--- src/main/java/com/studies/foodorders/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/studies/foodorders/core/openapi/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.studies.foodorders.core.openapi;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -16,7 +19,17 @@ public class SpringFoxConfig {
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("com.studies.foodorders.api"))
 					.paths(PathSelectors.any())
-					.build();
+					.build()
+				.apiInfo(apiInfo());
+	}
+
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Order Food API")
+				.description("Open API")
+				.version("1")
+				.contact(new Contact("Order Food", "https://www.domain.com", "contact@domain.com"))
+				.build();
 	}
 	
 }
