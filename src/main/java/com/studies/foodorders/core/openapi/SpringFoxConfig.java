c3485248466a54947cd7ca3da6d2b39b943da088
Rafhael <rafhael.dev@gmail.com>
2023-09-15 08:37:29 -0300

    feat: describing response status codes globally

--- src/main/java/com/studies/foodorders/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/studies/foodorders/core/openapi/SpringFoxConfig.java
@@ -3,15 +3,22 @@ package com.studies.foodorders.core.openapi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Response;
 import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @Import(BeanValidatorPluginsConfiguration.class)
@@ -24,8 +31,60 @@ public class SpringFoxConfig {
 					.apis(RequestHandlerSelectors.basePackage("com.studies.foodorders.api"))
 					.paths(PathSelectors.any())
 					.build()
+				.useDefaultResponseMessages(false)
+				.globalResponses(HttpMethod.GET, globalGetResponseMessages())
+				.globalResponses(HttpMethod.POST, globalPostPutResponseMessages())
+				.globalResponses(HttpMethod.PUT, globalPostPutResponseMessages())
+				.globalResponses(HttpMethod.DELETE, globalDeleteResponseMessages())
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cities", "Manage Cities"));
+	}
+
+	private List<Response> globalGetResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+						.code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+						.description("Resource does not have representation that can be accepted by the consumer")
+						.build(),
+				new ResponseBuilder()
+						.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+						.description("Internal Server Error")
+						.build()
+		);
+	}
+
+	private List<Response> globalPostPutResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+						.code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+						.description("Invalid request (Client error)")
+						.build(),
+				new ResponseBuilder()
+						.code(String.valueOf(HttpStatus.NOT_ACCEPTABLE.value()))
+						.description("Resource does not have representation that can be accepted by the consumer")
+						.build(),
+				new ResponseBuilder()
+						.code(String.valueOf(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value()))
+						.description("Request refused because the body is in an unsupported format")
+						.build(),
+				new ResponseBuilder()
+						.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+						.description("Internal Server Error")
+						.build()
+		);
+	}
+
+	private List<Response> globalDeleteResponseMessages() {
+		return Arrays.asList(
+				new ResponseBuilder()
+						.code(String.valueOf(HttpStatus.BAD_REQUEST.value()))
+						.description("Invalid request (Client error)")
+						.build(),
+				new ResponseBuilder()
+						.code(String.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value()))
+						.description("Internal Server Error")
+						.build()
+		);
 	}
 
 	public ApiInfo apiInfo() {
