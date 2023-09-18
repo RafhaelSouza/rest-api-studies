da9ff0b02ffaa2082c13a8eefd5b4544b39881c4
Rafhael <rafhael.dev@gmail.com>
2023-09-18 08:07:11 -0300

    feat: describing status codes for responses on specific endpoints

--- src/main/java/com/studies/foodorders/api/controllers/localization/CityController.java
+++ src/main/java/com/studies/foodorders/api/controllers/localization/CityController.java
@@ -1,15 +1,14 @@
 package com.studies.foodorders.api.controllers.localization;
 
 import com.studies.foodorders.api.converter.city.CityModelConverter;
+import com.studies.foodorders.api.exceptionhandler.ApiError;
 import com.studies.foodorders.api.model.localization.city.CityInput;
 import com.studies.foodorders.api.model.localization.city.CityModel;
 import com.studies.foodorders.domain.exceptions.BusinessException;
 import com.studies.foodorders.domain.exceptions.StateNotFoundException;
 import com.studies.foodorders.domain.models.localization.City;
 import com.studies.foodorders.domain.services.localization.CityService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -36,6 +35,10 @@ public class CityController {
     }
 
     @ApiOperation("Find a city by id")
+    @ApiResponses({
+            @ApiResponse(code = 400, message = "Invalid city id", response = ApiError.class),
+            @ApiResponse(code = 404, message = "City not found", response = ApiError.class)
+    })
     @GetMapping("/{id}")
     public CityModel find(
             @ApiParam(value = "City id", example = "1")
@@ -44,6 +47,9 @@ public class CityController {
     }
 
     @ApiOperation("Register a city")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Registered city"),
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CityModel save(
@@ -58,6 +64,10 @@ public class CityController {
     }
 
     @ApiOperation("Update a city")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Updated city"),
+            @ApiResponse(code = 404, message = "City not found", response = ApiError.class)
+    })
     @PutMapping("/{id}")
     public CityModel update(
             @ApiParam(value = "City id", example = "1")
@@ -75,6 +85,10 @@ public class CityController {
     }
 
     @ApiOperation("Delete a city")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Deleted city"),
+            @ApiResponse(code = 404, message = "City not found", response = ApiError.class)
+    })
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(
