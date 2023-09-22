dc5af000a4e83d7f9ae4bd406bc6ab130e8d3b04
Rafhael <rafhael.dev@gmail.com>
2023-09-22 09:19:54 -0300

    feat: documentation of restaurant association endpoints with payment methods

---
+++ src/main/java/com/studies/foodorders/api/openapi/controllers/RestaurantPaymentWayControllerOpenApi.java
@@ -0,0 +1,36 @@
+package com.studies.foodorders.api.openapi.controllers;
+
+import com.studies.foodorders.api.exceptionhandler.ApiError;
+import com.studies.foodorders.api.model.paymentway.PaymentWayModel;
+import io.swagger.annotations.*;
+
+import java.util.List;
+
+@Api(tags = "Restaurants")
+public interface RestaurantPaymentWayControllerOpenApi {
+
+    @ApiOperation("List payment methods associated with the restaurant")
+    @ApiResponses({
+            @ApiResponse(code = 404, message = "Restaurant not found", response = ApiError.class)
+    })
+    List<PaymentWayModel> list(@ApiParam(value = "Restaurant id", example = "1", required = true) Long restaurantId);
+
+    @ApiOperation("Restaurant association with payment method")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Association done successfully"),
+            @ApiResponse(code = 404, message = "Restaurant or payment method not found",
+                    response = ApiError.class)
+    })
+    void associate(@ApiParam(value = "Restaurant id", example = "1", required = true) Long restaurantId,
+                   @ApiParam(value = "Payment Way id", example = "1", required = true) Long paymentWayId);
+
+    @ApiOperation("Disassociation of restaurant with payment method")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Disassociation done successfully"),
+            @ApiResponse(code = 404, message = "Restaurant or payment method not found",
+                    response = ApiError.class)
+    })
+    void disassociate(@ApiParam(value = "Restaurant id", example = "1", required = true) Long restaurantId,
+                      @ApiParam(value = "Payment Way id", example = "1", required = true) Long paymentWayId);
+
+}
