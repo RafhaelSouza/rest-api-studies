ee41ed1be9663115443224bc79c623843938ef1d
Rafhael <rafhael.dev@gmail.com>
2023-09-26 13:19:25 -0300

    feat: documentation of group membership endpoints with users

--- src/main/java/com/studies/foodorders/api/controllers/security/UserGroupController.java
+++ src/main/java/com/studies/foodorders/api/controllers/security/UserGroupController.java
@@ -2,16 +2,18 @@ package com.studies.foodorders.api.controllers.security;
 
 import com.studies.foodorders.api.converter.security.GroupModelConverter;
 import com.studies.foodorders.api.model.security.group.GroupModel;
+import com.studies.foodorders.api.openapi.controllers.UserGroupControllerOpenApi;
 import com.studies.foodorders.domain.models.security.User;
 import com.studies.foodorders.domain.services.security.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users/{userId}/groups")
-public class UserGroupController {
+public class UserGroupController implements UserGroupControllerOpenApi {
 
     private UserService userService;
 
@@ -22,7 +24,7 @@ public class UserGroupController {
         this.groupModelConverter = groupModelConverter;
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<GroupModel> list(@PathVariable Long userId) {
         User user = userService.findIfExists(userId);
 
