373821012d326b2ea203d79a417699e1919edf4c
Rafhael <rafhael.dev@gmail.com>
2023-10-11 08:20:03 -0300

    feat: default version of the api when the media type is not provided

--- src/main/java/com/studies/foodorders/core/web/WebConfig.java
+++ src/main/java/com/studies/foodorders/core/web/WebConfig.java
@@ -3,6 +3,7 @@ package com.studies.foodorders.core.web;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,8 +16,11 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
 			.allowedMethods("*");
-//			.allowedOrigins("*")
-//			.maxAge(30);
+	}
+
+	@Override
+	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+		configurer.defaultContentType(MediaTypeVersions.V2_APPLICATION_JSON);
 	}
 
 	@Bean
