9ed21989356882b132afb1b41dd511a07ae5d515
Rafhael <rafhael.dev@gmail.com>
2023-10-13 15:21:55 -0300

    feat: in-memory user authentication

--- src/main/java/com/studies/foodorders/core/security/WebSecurityConfig.java
+++ src/main/java/com/studies/foodorders/core/security/WebSecurityConfig.java
@@ -1,14 +1,30 @@
 package com.studies.foodorders.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+				.withUser("basicUser1")
+				.password(passwordEncoder().encode("1234"))
+				.roles("BASIC")
+				.and()
+				.withUser("basicUser2")
+				.password(passwordEncoder().encode("1234"))
+				.roles("BASIC");
+	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -23,6 +39,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 			.and()
 				.csrf().disable();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 	
 }
