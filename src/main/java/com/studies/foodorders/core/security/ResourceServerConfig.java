0f8cd21705a93338c898e735ceaabcdb0f3e7d8b
Rafhael <rafhael.dev@gmail.com>
2023-10-24 07:55:12 -0300

    feat: configuring jwt validation in the resource

--- src/main/java/com/studies/foodorders/core/security/ResourceServerConfig.java
+++ src/main/java/com/studies/foodorders/core/security/ResourceServerConfig.java
@@ -1,14 +1,9 @@
 package com.studies.foodorders.core.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-
-import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -23,13 +18,6 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 				.cors()
 			.and()
 				.oauth2ResourceServer().jwt();
-	}
-
-	@Bean
-	public JwtDecoder jwtDecoder() {
-		var secretKey = new SecretKeySpec("OCUHYglYq2/0txtRaV+WGuqN7CLuCMnf6DpTTFgaUHP0vFa+24Y/OvcKRbQFWWE5".getBytes(), "HmacSHA256");
-
-		return NimbusJwtDecoder.withSecretKey(secretKey).build();
 	}
 	
 }
